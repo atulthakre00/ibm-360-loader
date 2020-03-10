0e313db1b1a05978234786f45edc33d7ba79d1ee
Atul Thakre <atulthakre00@gmail.com>
2020-03-10 09:16:27 +0530

    Reading File and Calculating Relative Location Of Instructions

--- main.c
+++ main.c
@@ -26,9 +26,38 @@ int main()
 {
     int n=30 , i = 0;
     loader l[n];
-    esd e[n];
-    txt t[n];
-    rld r[n];	
+    esd e[10];
     FILE *inp = fopen("loaderProg.txt","r");
-    FILE *esd = fopen("esd.txt","w");    
+    FILE *esd = fopen("esd.txt","w");
+    l[0].rel = 0;
+    int extrnLoc;
+    int entryLoc;
+    int commaCount;
+    while(!feof(inp)){
+        fscanf(inp,"%s\t%s\t%s",l[i].name,l[i].inst,l[i].arg);
+        if(strcmp(l[i].inst,"EXTRN")==0)
+            extrnLoc = i;
+        if(strcmp(l[i].inst,"ENTRY")==0)
+            entryLoc = i;
+        if(i>0 && strcmp(l[i-1].inst,"DC")==0){
+            int j = 0;
+            commaCount = 0;
+            int lenArg = strlen(l[i-1].arg);
+            while(j<lenArg){
+                if((l[i-1].arg)[j] == ',')
+                    commaCount += 1;
+                j++;
+            }
+            l[i].rel = l[i-1].rel + 4*(commaCount+1);
+
+        }
+        else if(i>0){
+            l[i].rel = l[i-1].rel;
+        }
+        printf("\n%d\t%s\t%s\t%s\n",l[i].rel,l[i].name,l[i].inst,l[i].arg);
+        i++;
+    }
+    int num = i;
+    txt t[30];
+
 }
